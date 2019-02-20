fbba38c5df1cb1df609c0ce3581624a04c4e12db
Hariprasath <31165531+hari4274@users.noreply.github.com>
2019-02-20 14:36:49 +0530

    rsync examples
    rsync examples

--- rsync_ssh_copy_commands.sh
+++ rsync_ssh_copy_commands.sh
@@ -7,3 +7,6 @@ ssh-keygen
 cat ~/.ssh/id_rsa.pub | ssh hari@192.168.0.127 "mkdir -p ~/.ssh && touch ~/.ssh/authorized_keys && chmod -R go= ~/.ssh && cat >> ~/.ssh/authorized_keys"
 
 rsync -e ssh hari@192.168.0.127:/home/hari/Desktop/service_start/SourceDevBackupRemote/* /home/hari/Desktop/service_start/LocalDevBackup/
+
+
+# Examples : https://www.fastwebhost.in/blog/13-rsync-command-examples-on-linux/
