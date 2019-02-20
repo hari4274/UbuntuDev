6f08bcdf0a826a44261a50faabe5c5ece9579fa3
Hariprasath <31165531+hari4274@users.noreply.github.com>
2019-02-20 14:34:30 +0530

    How To Set Up SSH Keys on Ubuntu
    How To Set Up SSH Keys on Ubuntu

---
+++ rsync_ssh_copy_commands.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# https://www.digitalocean.com/community/tutorials/how-to-set-up-ssh-keys-on-ubuntu-1604
+
+ssh-keygen
+
+cat ~/.ssh/id_rsa.pub | ssh hari@192.168.0.127 "mkdir -p ~/.ssh && touch ~/.ssh/authorized_keys && chmod -R go= ~/.ssh && cat >> ~/.ssh/authorized_keys"
+
+rsync -e ssh hari@192.168.0.127:/home/hari/Desktop/service_start/SourceDevBackupRemote/* /home/hari/Desktop/service_start/LocalDevBackup/
